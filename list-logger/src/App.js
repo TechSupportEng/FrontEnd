a099ed2019bf4207a4ae86ce09644e78d4adaf79
Shelby Diamond <Shelbydiamond518@gmail.com>
2020-03-16 20:10:51 -0700

    updated skeleton of routes and components to make

--- list-logger/src/App.js
+++ list-logger/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Link } from "react-router-dom"
+import Home from "./Components/Home"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/registration" component={Registration} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/view" component={ViewLogs} />
+        <Route exact path="/newlog" component={AddANewLog} />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
