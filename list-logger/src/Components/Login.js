f272882cf6ccb6ca3eae7f205ec1b95ceac2be11
Shelby Diamond <Shelbydiamond518@gmail.com>
2020-03-17 11:05:03 -0700

    building out the login page

--- list-logger/src/Components/Login.js
+++ list-logger/src/Components/Login.js
@@ -1,9 +1,45 @@
 import React from "react"
 
 export const Login = () => {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    password: ""
+  })
+
+  const changeHandler = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: [event.target.value]
+    })
+  }
+  const submitHandler = event => {
+    event.preventDefault()
+    axios
+      .post("", credentials)
+      .then(response => {
+        localStorage.setitem("token", response.data.key)
+        history.pushState("/view")
+      })
+      .catch(error => {
+        console.log("This is an error from the Login submitHandler", error)
+      })
+  }
+
   return (
     <div>
-      <h2>Obviously this is a Login page</h2>
+      <form onsubmit={submitHandler}>
+        <h2>Sign In</h2>
+        <label>Name</label>
+        <input
+          className="input"
+          type="text"
+          name="name"
+          placeholder="Wriggley"
+          onChange={changeHandler}
+          value={credentials.name}
+        />
+        <label>Password</label>
+      </form>
     </div>
   )
 }
