ebe355711df1640494aafa5bfc0cf373e9449edc
Shelby Diamond <Shelbydiamond518@gmail.com>
2020-03-18 16:46:34 -0700

    created a form for adding a new log
    styled login, Registration, forms

--- list-logger/src/Components/Login.js
+++ list-logger/src/Components/Login.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
+import "../Styles/forms.css"
+import { Link } from "react-router-dom"
 
 export const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -14,31 +17,57 @@ export const Login = () => {
   }
   const submitHandler = event => {
     event.preventDefault()
-    axios
-      .post("", credentials)
-      .then(response => {
-        localStorage.setitem("token", response.data.key)
-        history.pushState("/view")
-      })
-      .catch(error => {
-        console.log("This is an error from the Login submitHandler", error)
-      })
+    // axios
+    //   .post("", credentials)
+    //   .then(response => {
+    //     localStorage.setitem("token", response.data.key)
+    //     history.pushState("/view")
+    //   })
+    //   .catch(error => {
+    //     console.log("This is an error from the Login submitHandler", error)
+    //   })
   }
 
   return (
-    <div>
+    <div className="formContainer">
       <form onsubmit={submitHandler}>
         <h2>Sign In</h2>
-        <label>Name</label>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          placeholder="Wriggley"
-          onChange={changeHandler}
-          value={credentials.name}
-        />
-        <label>Password</label>
+        <div className="parent">
+          <label>Name</label>
+        </div>
+        <div className="parent">
+          <input
+            className="input"
+            type="text"
+            name="name"
+            placeholder="Wriggley"
+            onChange={changeHandler}
+            value={credentials.name}
+          />
+        </div>
+        <div className="parent">
+          <label>Password</label>
+        </div>
+        <div className="parent">
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="********"
+            onChange={changeHandler}
+            value={credentials.password}
+          />
+        </div>
+        <div className="parent">
+          <button onclick={submitHandler} className="button">
+            Submit
+          </button>
+        </div>
+        <div className="parent">
+          <Link to="/registration" className="parent">
+            Need An Account?
+          </Link>
+        </div>
       </form>
     </div>
   )
--- list-logger/src/Components/ViewLogs.js
+++ list-logger/src/Components/ViewLogs.js
@@ -1,12 +1,98 @@
 import React from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+import "../Styles/forms.css"
+import "../Styles/logform.css"
 
 export const ViewLogs = () => {
+  const submitHandler = event => {
+    event.preventDefault()
+    // axios
+    //   .get("")
+    //   .then(response => {
+    //     console.log(response.data)
+    //   })
+    //   .catch(error => {
+    //     console.log("This is an error from the ViewLogs", error)
+    //   })
+  }
+
   return (
-    <div>
-      <h2>
-        This should be a Get request to list off the cards of all the previous
-        logs entered
-      </h2>
+    <div className="formContainer">
+      <form className="log-form" onsubmit={submitHandler}>
+        <h2>T-SET LOG REPORT</h2>
+        <div className="parents">
+          <label>TIME</label>
+          <input
+            className="inputs"
+            type="text"
+            name="name"
+            placeholder="17:30"
+            //onChange={changeHandler}
+            //value={name}
+          />
+        </div>
+        <div className="parents">
+          <label>LOG NAME</label>
+          <input
+            className="inputs"
+            type="text"
+            name="logName"
+            placeholder="Mia-Gateway Dashboard"
+            //onChange={changeHandler}
+            // value={logName}
+          />
+        </div>
+        <div className="parents">
+          <label>INCIDENT ID</label>
+          <input
+            className="inputs"
+            type="text"
+            name="incidentId"
+            placeholder="Incident Number or Time of Incident"
+            //onChange={changeHandler}
+            //value={incidentNumber}
+          />
+        </div>
+        <div className="parents">
+          <label>STATUS</label>
+          <input
+            className="inputs"
+            type="text"
+            name="status"
+            placeholder="Needs Follow Up || LGTM"
+            //onChange={changeHandler}
+            //value={incidentNumber}
+          />
+        </div>
+        <div className="parents">
+          <label>Dicoveries</label>
+          <input
+            className="inputs"
+            type="text"
+            name="discoveries"
+            placeholder="What did you research"
+            //onChange={changeHandler}
+            //value={incidentNumber}
+          />
+        </div>
+        <div className="parents">
+          <label>Additional Comments</label>
+          <input
+            className="inputs"
+            type="text"
+            name="discoveries"
+            placeholder="Is there a pattern? Anything extra we might need to know in the future looking back?"
+            //onChange={changeHandler}
+            //value={incidentNumber}
+          />
+        </div>
+        <div className="parent">
+          <button onclick={submitHandler} className="button">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
